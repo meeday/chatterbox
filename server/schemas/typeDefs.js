d57feaf0876f60d1c463f1557177f0a06596f7fd
Meedaxa Ahmed <meedaxa.ahmed@gmail.com>
2023-07-05 21:06:15 +0100

    add typedefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,60 @@
+const { gql } = require("apollo-server-express");
+
+const typeDefs = gql`
+  type User {
+    _id: ID!
+    username: String!
+    email: String
+    avatar: String
+    bio: String
+  }
+
+  type Chat {
+    _id: ID!
+    chatName: String
+    isGroupChat: Boolean
+    users: [User]
+    latestMessage: Message
+    groupAdmin: User
+  }
+
+  type Message {
+    _id: ID!
+    sender: User
+    message: String
+    chat: Chat
+  }
+
+  type Auth {
+    token: ID!
+    user: User
+  }
+
+  input UserInput {
+    username: String!
+    email: String!
+    password: String!
+    avatar: String
+    bio: String
+  }
+
+  type Query {
+    me: User
+    searchUser(username: String!): [User]
+    getChat(userId: ID!): Chat
+    getAllChats: [Chat]
+    getAllMessages(chatId: ID!): [Message]
+  }
+
+  type Mutation {
+    login(username: String!, password: String!): Auth
+    signup(userData: UserInput!): Auth
+    createChat(users: [String], chatName: String!): Chat
+    renameChat(chatId: ID!, chatName: String!): Chat
+    addUserToChat(chatId: ID!, userId: ID!): Chat
+    removeUserFromChat(chatId: ID!, userId: ID!): Chat
+    sendMessage(message: String!, chatId: ID!): Message
+  }
+`;
+
+module.exports = typeDefs;
